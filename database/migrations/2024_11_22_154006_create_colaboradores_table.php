871bb9c4fac00a6999a46e5099c7fe9a1e70c48f
Sinomar <nomarpereira.2015@gmail.com>
2024-11-22 16:03:53 -0300

    Criiado o migration colaboradores

---
+++ database/migrations/2024_11_22_154006_create_colaboradores_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('colaboradores', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->string('email')->unique();
+            $table->string('telefone');
+            $table->string('cidade');
+            $table->string('estado', 2);
+            $table->string('numero');
+            $table->string('bairro');
+            $table->string('cargo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('colaboradores');
+    }
+};
